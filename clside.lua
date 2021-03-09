b19c94e3c719df4ccc3f064138b416af72deeaf6
SynTer <alpacanismo@gmail.com>
2021-03-09 16:51:29 -0300

    Update clside.lua

--- clside.lua
+++ clside.lua
@@ -1,11 +1,14 @@
 SetDiscordAppId(cfg.DiscordAppID)
 
 Citizen.CreateThread(function()
-	SetDiscordRichPresenceAsset(cfg.discordImageName) 
-	SetDiscordRichPresenceAssetText(cfg.hoverText) 
-	SetDiscordRichPresenceAssetSmall(cfg.smallDiscordImageName) -- Name of the smaller image asset.
-	SetDiscordRichPresenceAssetSmallText(cfg.smallHoverText)
-	SetRichPresence(cfg.richPresenceText) 
-	SetDiscordRichPresenceAction(0, cfg.button1.text, cfg.button1.url)
-	SetDiscordRichPresenceAction(1, cfg.button2.text, cfg.button2.url)
+	while true do
+		SetDiscordRichPresenceAsset(cfg.discordImageName) 
+		SetDiscordRichPresenceAssetText(cfg.hoverText) 
+		SetDiscordRichPresenceAssetSmall(cfg.smallDiscordImageName) -- Name of the smaller image asset.
+		SetDiscordRichPresenceAssetSmallText(cfg.smallHoverText)
+		SetRichPresence(cfg.richPresenceText) 
+		SetDiscordRichPresenceAction(0, cfg.button1.text, cfg.button1.url)
+		SetDiscordRichPresenceAction(1, cfg.button2.text, cfg.button2.url)
+		Wait(5000)
+	end
 end)
